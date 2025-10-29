83adcee68c4608885eeffcf88d19a8545c8e08f5
sanchezjulietam <sanchezjulietam@gmail.com>
2025-10-28 21:08:08 -0300

    pantalla Formulario Registro

---
+++ TP-Cuatrimestral-Equipo-5A/Presentacion/FormularioRegistro.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="FormularioRegistro.aspx.cs" Inherits="Presentacion.FormularioRegistro" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container mt-4">
+        <h3 class="ms-3 mb-3">Formulario de registro</h3>
+        <h4 class="text-primary ms-3 mb-4">Datos personales</h4>.
+
+        <div class="d-flex align-items-center mb-4">
+            <div class="position-relative" style="width:80px; height:80px;">
+               <asp:Image ID="imgPreview" runat="server" CssClass="rounded-circle w-100 h-100" ImageUrl="~images/default-profile.png" />
+
+                <asp:FileUpload ID="fuImagen" runat="server" CssClass="d-none" OnChage="Imagen_Changed" />
+                <label for="fuImagen" class="btn btn-link p-0 position-absolute" style="bottom:0; left:100%;">Cambiar foto</label>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-md-6">
+                <label for="txtNombre" class="form-label">Nombre</label>
+                <asp:TextBox runat="server" ID="txtNombre" CssClass="form-control" placeholder="Nombre"></asp:TextBox>
+            </div>
+            <div class="col-md-6">
+                <label for="txtApellido" class="form-label">Apellido</label>
+                <asp:TextBox runat="server" ID="txtApellido" CssClass="form-control" placeholder="Apellido"></asp:TextBox>
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <label for="TextFechaNacimiento" class="form-label">Fecha de nacimiento</label>
+            <asp:TextBox ID="TextFechaNacimiento" runat="server" CssClass="form-control" TextMode="Date"></asp:TextBox>
+        </div>
+
+        <div class="mb-3">
+            <label for="txtEmail" class="form-label">Email</label>
+            <div class="input-group">
+                <span class="input-group-text">@</span>
+                <asp:TextBox runat="server" ID="txtEmail" CssClass="form-control"></asp:TextBox>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-md-6">
+                <label for="txtDocumento" class="form-label">Documento</label>
+                <asp:TextBox runat="server" ID="txtDocumento" CssClass="form-control" placeholder="Documento"></asp:TextBox>
+            </div>
+            <div class="col-md-6">
+                <label for="TextTelefono" class="form-label">Teléfono</label>
+                <asp:TextBox runat="server" ID="TextTelefono" CssClass="form-control" placeholder="Teléfono"></asp:TextBox>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            
+                <label class="form-label d-block mb-2">Tipo de usuario</label>
+
+                <div class="form-check w-100 mb-2 ms-2">
+                    <asp:RadioButton ID="rbMedico" runat="server" GroupName="TipoUsuario" CssClass="form-check-input" />
+                    <label for="rbMedico" class="form-check-label ms-2">Médico</label>
+                </div>
+
+                <div class="form-check w-100 mb-2 ms-2">
+                    <asp:RadioButton ID="rbPaciente" runat="server" GroupName="TipoUsuario" CssClass="form-check-input" />
+                    <label for="rbPaciente" class="form-check-label ms-2">Paciente</label>
+                </div>
+
+                <div class="form-check w-100 mb-2 ms-2">
+                    <asp:RadioButton ID="rbPersonal" runat="server" GroupName="TipoUsuario" CssClass="form-check-input" />
+                    <label for="rbPersonal" class="form-check-label ms-2">Personal</label>
+                </div>
+       
+        </div>
+
+        <div class="row mb-3">
+            <asp:Button Text="Registrarse" ID="BtnRegistrarse" OnClick="BtnRegistrarse_Click" runat="server" class="btn btn-primary" />
+        </div>
+    </div>
+
+</asp:Content>
---
+++ TP-Cuatrimestral-Equipo-5A/Presentacion/FormularioRegistro.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentacion
+{
+
+
+    public partial class FormularioRegistro
+    {
+
+        /// <summary>
+        /// Control imgPreview.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgPreview;
+
+        /// <summary>
+        /// Control fuImagen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload fuImagen;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellido;
+
+        /// <summary>
+        /// Control TextFechaNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextFechaNacimiento;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Control txtDocumento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDocumento;
+
+        /// <summary>
+        /// Control TextTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextTelefono;
+
+        /// <summary>
+        /// Control rbMedico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbMedico;
+
+        /// <summary>
+        /// Control rbPaciente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbPaciente;
+
+        /// <summary>
+        /// Control rbPersonal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbPersonal;
+
+        /// <summary>
+        /// Control BtnRegistrarse.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnRegistrarse;
+    }
+}
