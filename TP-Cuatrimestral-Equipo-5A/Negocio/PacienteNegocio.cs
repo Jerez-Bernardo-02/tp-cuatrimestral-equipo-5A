9af405b645bd9e9ddae9abd61a9463e2ec3564ae
Jerez-Bernardo-02 <jerez.bernardo.02@gmail.com>
2025-10-29 21:02:23 -0300

    PacienteNegocio - Método Listar

---
+++ TP-Cuatrimestral-Equipo-5A/Negocio/PacienteNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Dominio;
+
+namespace Negocio
+{
+    public class PacienteNegocio
+    {
+        public List<Paciente> listar()
+        {
+            List<Paciente> lista = new List<Paciente>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT  P.Id AS IdPaciente, Pe.Id AS IdPersona, Pe.Nombre, Pe.Apellido, Pe.FechaNacimiento, Pe.Dni, Pe.Email, Pe.Telefono, Pe.UrlImagen, Pe.Activo FROM Pacientes P INNER JOIN Personas Pe ON P.IdPersona = Pe.Id;");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Paciente aux = new Paciente();
+
+                    aux.Id = (int)datos.Lector["IdPaciente"];
+
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Apellido = (string)datos.Lector["Apellido"];
+                    aux.FechaNacimiento = (DateTime)datos.Lector["FechaNacimiento"];
+                    aux.Documento = (string)datos.Lector["Dni"];
+                    aux.Email = (string)datos.Lector["Email"];
+                    aux.Telefono = datos.Lector["Telefono"] != DBNull.Value ? (string)datos.Lector["Telefono"] : null;
+                    aux.UrlImagen = datos.Lector["UrlImagen"] != DBNull.Value ? (string)datos.Lector["UrlImagen"] : null;
+                    aux.Activo = (bool)datos.Lector["Activo"];
+
+                    lista.Add(aux);
+                }
+
+                datos.cerrarConexion();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
