66420cb5a53caf1df6b2c0402ac317935ae60b3f
MarcosMazzitelli <marcosmazzitelli89@gmail.com>
2025-11-02 21:52:55 -0300

    Actualización Script DB

--- TP-Cuatrimestral-Equipo-5A/Dominio/HorarioPorMedico.cs
+++ TP-Cuatrimestral-Equipo-5A/Dominio/HorarioPorMedico.cs
@@ -9,6 +9,7 @@ namespace Dominio
     internal class HorarioPorMedico
     {
         public int Id { get; set; }
+        public int IdMedico { get; set; }
         public int DiaSemana { get; set; }
         public TimeSpan HoraEntrada { get; set; }//para representar una hora del dia
         public TimeSpan HoraSalida { get; set; }
